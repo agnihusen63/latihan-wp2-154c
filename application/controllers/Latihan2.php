34303971ee023227b5c13adf6fa1ea52b501fad4
MuhammadRizqi06 <anakjatijajar@gmail.com>
2022-03-31 14:45:21 +0700

    Tugas Web 2 Pertemuan 3

--- application/controllers/Latihan2.php
+++ application/controllers/Latihan2.php
@@ -1,22 +1,37 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Latihan2 extends CI_Controller {
+class Latihan2 extends CI_Controller
+{
 
-    public function index() {        
+    public function index()
+    {
         $this->load->helper('url');
         $this->load->view('input_matakuliah.php');
     }
 
-    public function cetak(){
+    public function cetak()
+    {
         $kodeMataKuliah = $this->input->post('kode');
         $namaMataKuliah = $this->input->post('nama');
         $sksMataKuliah = $this->input->post('sks');
 
-        if($sksMataKuliah== 3 || $sksMataKuliah == 4){
+        if ($sksMataKuliah == 4) {
             $sksUnggulan = "Unggulan SKS";
-        }else {
+            $bobotnilai = "81 - 100";
+            $status = "Tidak Remedial";
+        } else if ($sksMataKuliah == 3) {
             $sksUnggulan = "SKS regular";
+            $bobotnilai = "75 - 80";
+            $status = "Tidak Remedial";
+        } else if ($sksMataKuliah == 2) {
+            $sksUnggulan = "SKS regular";
+            $bobotnilai = "61 - 74";
+            $status = "Tidak Remedial";
+        } else if ($sksMataKuliah == 1) {
+            $sksUnggulan = "SKS regular";
+            $bobotnilai = "Nilai Dibawah 60";
+            $status = "Remedial";
         }
 
         //membuat object untuk parsing data ke view yg dituju
@@ -24,13 +39,12 @@ class Latihan2 extends CI_Controller {
             'kode' => $kodeMataKuliah,
             'nama' => $namaMataKuliah,
             'sks' => $sksMataKuliah,
-            'unggulan' => $sksUnggulan
+            'unggulan' => $sksUnggulan,
+            'range' => $bobotnilai,
+            'Status' => $status
         ];
 
         //kirim ke view
         $this->load->view('output_matakuliah.html', $data);
-
-
     }
-
 }
